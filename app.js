6c550cacacd8e5efa79cfb3d7082f6509b6fb70c
zhongzefang001 <zhongzefang001@ke.com>
2021-08-11 22:16:04 +0800

    static

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
+const send = require('koa-send')
+const path = require('path')
 
 const routes = require('./routes')
 
@@ -12,7 +14,17 @@ app.use(cors());
 app.use(bodyParser());
 
 app.use(async (ctx, next) => {
-  ctx.response.status = 200;
+  const url = ctx.request.url
+  if (url.startsWith('/swagger')) {
+    ctx.response.status = 200;
+    const filePath = path.resolve(__dirname, url)
+    await send(ctx, filePath)
+  } else {
+    await next()
+  }
+})
+
+app.use(async (ctx, next) => {
   await next();
 });
 
